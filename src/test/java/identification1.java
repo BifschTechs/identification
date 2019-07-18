1652eae91d2b0f45d0d4394e20638f8dc67fdfc5
BifschTechs <max769650@gmail.com>
2019-07-18 14:23:03 -0500

    second identity +2.

--- src/test/java/identification1.java
+++ src/test/java/identification1.java
@@ -3,5 +3,6 @@ public class identification1 {
     public static void main(String[] args) {
 
         System.out.println("first identity + 1");
+        System.out.println("second identity +2");
     }
 }
